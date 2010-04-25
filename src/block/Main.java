26c1210da25df81c0ebc2d07d4f82685a8c1b104
PaulineC <pauline.chamoreau@gmail.com>
2010-04-25 23:46:21 +0200

    maj Block

--- src/block/Main.java
+++ src/block/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		BlockImpl impl = new BlockImpl();
 		BlockContract contract = new BlockContract(impl);
 		
-		contract.init(3);
+		contract.init('L');
 		System.out.println("NbPos = "+contract.getNbPos());
 		System.out.println("Size = "+contract.getSize());
 		
@@ -20,8 +20,7 @@ public class Main {
 		System.out.println("YMin = "+contract.getYMin());
 		System.out.println("YMax = "+contract.getYMax());
 		
-System.out.println ("LowPos");
-		
+		System.out.println ("LowPos");
 		Set<LinkedList<Integer>> getLowPos = contract.getLowPos();
 		Iterator<LinkedList<Integer>> it = getLowPos.iterator();
 		LinkedList<Integer> paire;
@@ -30,22 +29,7 @@ System.out.println ("LowPos");
 			System.out.println("x = "+paire.getFirst()+" y = "+paire.getLast());
 		}
 		
-		contract.addPos(2, 1);
-		contract.addPos(2, 2);
-		contract.addPos(2, 3);
-		contract.addPos(3, 3);
-		
-		
-		System.out.println("\nNbPos = "+contract.getNbPos());
-		System.out.println("Size = "+contract.getSize());
-		
-		System.out.println("XMin = "+contract.getXMin());
-		System.out.println("XMax = "+contract.getXMax());
-		System.out.println("YMin = "+contract.getYMin());
-		System.out.println("YMax = "+contract.getYMax());
-		
 		System.out.println("AllPos");
-		
 		Set<LinkedList<Integer>> getAllPos = contract.getAllPos();
 		 it = getAllPos.iterator();
 		while(it.hasNext()) {
@@ -53,14 +37,24 @@ System.out.println ("LowPos");
 			System.out.println("x = "+paire.getFirst()+" y = "+paire.getLast());
 		}
 		
-		System.out.println ("LowPos");
+		contract.rotateRight();
 		
+		System.out.println ("\n\nLowPos");
 		getLowPos = contract.getLowPos();
 		it = getLowPos.iterator();
 		while(it.hasNext()) {
 			paire = it.next();
 			System.out.println("x = "+paire.getFirst()+" y = "+paire.getLast());
 		}
+		
+		System.out.println("AllPos");
+		getAllPos = contract.getAllPos();
+		 it = getAllPos.iterator();
+		while(it.hasNext()) {
+			paire = it.next();
+			System.out.println("x = "+paire.getFirst()+" y = "+paire.getLast());
+		}
+
 	}
 
 }
