70476681ba5acc04ad6b2043c10c41711a23a073
NicolasR <nicolasrig@aol.com>
2010-05-09 16:23:52 +0200

    Mise à jour tests Board

--- src/board/BoardTest.java
+++ src/board/BoardTest.java
@@ -1,6 +1,7 @@
 package board;
 
 import static org.junit.Assert.*;
+import grid.GridContract;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,70 +23,65 @@ public class BoardTest{
 	public void testinit(){
 		board = new BoardContract(impl);
 		try{
+			board.init(-1,10);
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}
+		try{
+			board.init(10,-1);
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}
+		try{
 			board.init(10,9);
 			assertTrue(false);
 		}catch(Error e){
 			assertTrue(true);
 		}
 		
-		board = new BoardContract(new BoardImpl());
 		try{
 			board.init(10,22);
 			assertTrue(true);
 		}catch(Error e){
 			assertTrue(false);
 		}
-		boolean oracle_post = (board.getgrid().getWidth() == 10) && (board.getgrid().getHeight() == 22);
-		assertTrue(oracle_post);	
 		
-		assertFalse(board.isBlock());
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
 		
+		assertTrue(board.isBlock() == false);
+		assertTrue(board.getgrid().getWidth() == 10 && board.getgrid().getHeight() == 22);
 	}
 	
-	@Test
-	public void testinsert(){
-		board = new BoardContract(impl);
-		board.init(10, 22);
-		
-		boolean oracle_pre = (!board.isBlock());
-		assertTrue(oracle_pre);
-		BlockContract block = new BlockContract(new BlockImpl());
-		
-		try{
-			block.init('O');
-			board.insert(block);
-			assertTrue(true);
-		}catch(Error e){
-			assertTrue(false);
-		}
-		boolean oracle_post = (board.isBlock() && board.getcurrentBlock() == block 
-				&& board.getXMinBlock()>0 && board.getYMinBlock() == 1);
-		assertTrue(oracle_post);
-		
-		try{
-			block = new BlockContract(new BlockImpl());
-			board.insert(block);
-			assertTrue(false);
-		}catch(Error e){
-			assertTrue(true);
-		}
-	}
 	
 	@Test
 	public void testdoRotateLeft(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.doRotateLeft();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
 		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.canRotateLeft() && board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		int getNbLastCleaned_atPre = board.getNbLastCleaned();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		
 		try{
-			board.doRotateLeft();
 			board.doRotateLeft();
 			board.doRotateLeft();
 			board.doRotateLeft();
@@ -93,26 +89,44 @@ public class BoardTest{
 		}catch(Error e){
 			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == getXMinBlock_atPre)
-			&& (board.getYMinBlock() == getYMinBlock_atPre);
 		
-		assertTrue(oracle_post);
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.getNbLastCleaned() == getNbLastCleaned_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre);
 	}
+	
 	
 	@Test
 	public void testdoLeft(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.doLeft();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
-		block.init('O');
+		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.cangoLeft() && board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		int getNbLastCleaned_atPre = board.getNbLastCleaned();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		
 		try{
 			board.doLeft();
 			board.doLeft();
@@ -121,35 +135,45 @@ public class BoardTest{
 		}catch(Error e){
 			assertTrue(false);
 		}
-		try{
-			board.doLeft();
-			board.doLeft();
-			board.doLeft();
-			assertTrue(false);
-		}catch(Error e){
+		
+		try {
+			board.checkInvariants();
 			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == (getXMinBlock_atPre-5))
-			&& (board.getYMinBlock() == getYMinBlock_atPre);
-		assertTrue(oracle_post);
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.getNbLastCleaned() == getNbLastCleaned_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre-3);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre);
 	}
+	
 	
 	@Test
 	public void testdoRotateRight(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.doRotateRight();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
 		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.canRotateRight() && board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		int getNbLastCleaned_atPre = board.getNbLastCleaned();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		
 		try{
-			board.doRotateRight();
 			board.doRotateRight();
 			board.doRotateRight();
 			board.doRotateRight();
@@ -157,26 +181,44 @@ public class BoardTest{
 		}catch(Error e){
 			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == getXMinBlock_atPre)
-			&& (board.getYMinBlock() == getYMinBlock_atPre);
 		
-		assertTrue(oracle_post);
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.getNbLastCleaned() == getNbLastCleaned_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre);
 	}
 	
+
 	@Test
-	public void testdoRight(){	
+	public void testdoRight(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.doRight();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
-		block.init('O');
+		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.cangoRight() && board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		int getNbLastCleaned_atPre = board.getNbLastCleaned();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		
 		try{
 			board.doRight();
 			board.doRight();
@@ -185,102 +227,187 @@ public class BoardTest{
 		}catch(Error e){
 			assertTrue(false);
 		}
-		try{
-			board.doRight();
-			assertTrue(false);
-		}catch(Error e){
+		
+		try {
+			board.checkInvariants();
 			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == (getXMinBlock_atPre+3))
-			&& (board.getYMinBlock() == getYMinBlock_atPre);
-
-		assertTrue(oracle_post);
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.getNbLastCleaned() == getNbLastCleaned_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre+3);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre);
 	}
-	
+
+		
 	@Test
 	public void testdoBottom(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.doBottom();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
-		block.init('O');
+		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre, getBottomHeight_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();	
-		getBottomHeight_atPre = board.getBottomHeight();
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		int getBottomHeight_atPre = board.getBottomHeight();
+		
 		try{
 			board.doBottom();
 			assertTrue(true);
 		}catch(Error e){
-			e.printStackTrace();
 			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == (getXMinBlock_atPre))
-			&& (board.getYMinBlock() == getYMinBlock_atPre + getBottomHeight_atPre);
 		
-		assertTrue(oracle_post);
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre+getBottomHeight_atPre);
 	}
 	
+
 	@Test
 	public void testStep(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.step();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
-		block.init('O');
+		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.isBlock();
-		assertTrue(oracle_pre);
-		int getXMinBlock_atPre, getYMinBlock_atPre;
-		getXMinBlock_atPre = board.getXMinBlock();
-		getYMinBlock_atPre = board.getYMinBlock();	
+		GridContract getgrid_atPre = board.getgrid();
+		boolean isBlock_atPre = board.isBlock();
+		int getXMinBlock_atPre = board.getXMinBlock();
+		int getYMinBlock_atPre = board.getYMinBlock();
+		int getBottomHeight_atPre = board.getBottomHeight();
+		
 		try{
 			board.step();
 			assertTrue(true);
 		}catch(Error e){
 			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getXMinBlock() == (getXMinBlock_atPre))
-			&& (board.getYMinBlock() == getYMinBlock_atPre+1);
 		
-		assertTrue(oracle_post);
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.isBlock() == isBlock_atPre);
+		assertTrue(board.getXMinBlock() == getXMinBlock_atPre);
+		assertTrue(board.getYMinBlock() == getYMinBlock_atPre+1);
+		assertTrue(board.getBottomHeight() == getBottomHeight_atPre-1);
 	}
 		
+	
+	@Test
+	public void testinsert(){
+		board = new BoardContract(impl);
+		board.init(10, 22);
+		BlockContract block = new BlockContract(new BlockImpl());
+		block.init('L');
+		board.insert(block);
+		BlockContract block2 = new BlockContract(new BlockImpl());
+		block2.init('J');
+		try{
+			board.insert(block2);
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
+		
+		board.remove();
+		
+		GridContract getgrid_atPre = board.getgrid();
+		
+		try{
+			board.insert(block2);
+			assertTrue(true);
+		}catch(Error e){
+			assertTrue(false);
+		}
+		
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == block2);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.isBlock());
+	}
+	
+	
 	@Test
 	public void testremove(){
 		board = new BoardContract(impl);
 		board.init(10, 22);
+		try{
+			board.remove();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}		
 		BlockContract block = new BlockContract(new BlockImpl());
-		block.init('O');
+		block.init('L');
 		board.insert(block);
 		
-		boolean oracle_pre = board.isBlock();
-		assertTrue(oracle_pre);
+		GridContract getgrid_atPre = board.getgrid();
+		int getNbLastCleaned_atPre = board.getNbLastCleaned();
 		
 		try{
 			board.remove();
-			assertTrue(true);
 		}catch(Error e){
 			assertTrue(false);
 		}
 		
-		boolean oracle_post = !board.isBlock();
-		assertTrue(oracle_post);
-		
-		try{
-			board.remove();
-			assertTrue(false);
-		}catch(Error e){
+		try {
+			board.checkInvariants();
 			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
 		}
+		
+		assertTrue(board.getcurrentBlock() == null);
+		assertTrue(board.getgrid() == getgrid_atPre);
+		assertTrue(board.getNbLastCleaned() == getNbLastCleaned_atPre);
+		assertTrue(board.isBlock() == false);
+		assertTrue(board.getXMinBlock() == 0);
+		assertTrue(board.getYMinBlock() == 0);
 	}
 	
+
 	@Test
 	public void testclean(){
 		board = new BoardContract(impl);
@@ -288,61 +415,91 @@ public class BoardTest{
 		BlockContract block = new BlockContract(new BlockImpl());
 		block.init('O');
 		board.insert(block);
-
-		board.doBottom();
-
-		boolean oracle_pre = board.isBottom();
-		assertTrue(oracle_pre);
-		int getNbLastCleaned_atPre = board.getNbLastCleaned();	
 		try{
 			board.clean();
-			assertTrue(true);
+			assertTrue(false);
 		}catch(Error e){
-			e.printStackTrace();
+			assertTrue(true);
+		}		
+		
+		board.doBottom();
+		
+		BlockContract getcurrentBlock_atPre = board.getcurrentBlock();
+		GridContract getgrid_atPre = board.getgrid();
+		
+		try{
+			board.clean();
+		}catch(Error e){
 			assertTrue(false);
 		}
-		 
-		boolean oracle_post = (board.getNbLastCleaned() >= getNbLastCleaned_atPre);
-		assertTrue(oracle_post);
+		
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
 		
 		
 		board = new BoardContract(impl);
 		board.init(10, 22);
 		board.insert(block);
-			
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-		board.step();
-			
-		oracle_pre = board.isBottom();
-		assertTrue(oracle_pre);
-		getNbLastCleaned_atPre = board.getNbLastCleaned();	
 		try{
 			board.clean();
-			assertTrue(true);
+			assertTrue(false);
 		}catch(Error e){
-			e.printStackTrace();
+			assertTrue(true);
+		}		
+		
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		board.step();
+		
+		try{
+			board.clean();
+			assertTrue(false);
+		}catch(Error e){
+			assertTrue(true);
+		}
+		
+		board.step();
+		
+		getcurrentBlock_atPre = board.getcurrentBlock();
+		getgrid_atPre = board.getgrid();
+		
+		try{
+			board.clean();
+		}catch(Error e){
 			assertTrue(false);
 		}
-		 
-		oracle_post = (board.getNbLastCleaned() >= getNbLastCleaned_atPre);
-		assertTrue(oracle_post);
+		
+		try {
+			board.checkInvariants();
+			assertTrue(true);
+		} catch (Error err) {
+			assertTrue(false);
+		}
+		
+		assertTrue(board.getcurrentBlock() == getcurrentBlock_atPre);
+		assertTrue(board.getgrid() == getgrid_atPre);
 	}
 }
